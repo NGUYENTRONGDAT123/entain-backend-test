2153f701439c7c6a1af4c4d91e07b1080e59f0ee
Trong Dat <datnguyentrong98@gmail.com>
2023-04-28 15:46:12 +1000

    Add error 404 not found if it could not found the desire race

--- racing/test/races_test.go
+++ racing/test/races_test.go
@@ -13,6 +13,8 @@ import (
 	"git.neds.sh/matty/entain/racing/db"
 	"git.neds.sh/matty/entain/racing/proto/racing"
 	"git.neds.sh/matty/entain/racing/service"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/protobuf/proto"
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
@@ -440,5 +442,58 @@ func TestGetRace_Default(t *testing.T) {
 	}
 	if !proto.Equal(resp, expectedRace) {
 		t.Errorf("Response did not match expected value. Got %v, expected %v", resp, expectedRace)
+	}
+}
+
+func TestGetRace_IdIsNotFound(t *testing.T) {
+	// Set up a test database with for testing
+	racingDB, err := NewTestDB()
+	defer racingDB.Close()
+
+	// clear the data
+	racingDB.Exec(getRaceQueriesForTest()[clearAllDataRace])
+
+	// Set up a new RacingService with the test database
+	racesRepo := db.NewRacesRepo(racingDB)
+	racingService := service.NewRacingService(racesRepo)
+
+	timeTest1, err := time.Parse(time.RFC3339, "2000-04-05T00:00:00Z")
+	timeTest2, err := time.Parse(time.RFC3339, "2001-04-05T00:00:00Z")
+	timeTest3, err := time.Parse(time.RFC3339, "2002-04-05T00:00:00Z")
+	// Insert a race record into the races table
+	InsertNewRace(&racing.Race{
+		Id:                  1,
+		MeetingId:           1,
+		Name:                "Test Race 1",
+		Number:              1,
+		Visible:             true,
+		AdvertisedStartTime: timestamppb.New(timeTest1),
+	}, racingDB, t)
+	InsertNewRace(&racing.Race{
+		Id:                  2,
+		MeetingId:           2,
+		Name:                "Test Race 2",
+		Number:              2,
+		Visible:             false,
+		AdvertisedStartTime: timestamppb.New(timeTest2),
+	}, racingDB, t)
+	InsertNewRace(&racing.Race{
+		Id:                  3,
+		MeetingId:           3,
+		Name:                "Test Race 3",
+		Number:              3,
+		Visible:             true,
+		AdvertisedStartTime: timestamppb.New(timeTest3),
+	}, racingDB, t)
+
+	// Set up a new context and request for the GetRace RPC
+	ctx := context.Background()
+	req := &racing.GetRaceRequest{Id: 99}
+
+	// Call the GetRace RPC
+	_, err = racingService.GetRace(ctx, req)
+	// Check that the error is the expected type
+	if grpc.Code(err) != codes.NotFound {
+		t.Errorf("Expected error code %v but got %v", codes.NotFound, grpc.Code(err))
 	}
 }
